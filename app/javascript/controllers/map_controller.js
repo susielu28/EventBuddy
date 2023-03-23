4c738ccc102840d43ca69ff387ea1f9f59ca5f16
lukas noetzel <lukas.noetzel@gmail.com>
2023-03-23 11:33:17 +0100

    added markers and search

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -15,6 +16,8 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
