08e99ee8e4d2e9edf764832ee7767148b61e3430
rui pinto <rui_teixeirapinto@hotmail.com>
2023-03-29 16:13:53 +0100

    frontend homepage work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @elton = Event.where("venue ILIKE '%london%'").where("name ILIKE '%elton%'").first
+    @brighton = Event.where("venue ILIKE '%london%'").where("name ILIKE '%tottenham%' AND name ILIKE '%brighton%'").first
+    @rockofages = Event.where("venue ILIKE '%berlin%'").where("name ILIKE '%rock%'").first
   end
 end
